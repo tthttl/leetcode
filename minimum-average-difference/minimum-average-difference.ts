50652dbd49d4eae0e0c977570bfcb34ab2c698a0
Tóth Attila <tthttl@gmail.com>
2022-10-03 19:52:28 +0200

    Time: 226 ms, Memory: 58.4 MB - LeetHub

--- minimum-average-difference/minimum-average-difference.ts
+++ minimum-average-difference/minimum-average-difference.ts
@@ -1,4 +1,4 @@
-function minimumAverageDifference(nums: number[]): number {
+ function minimumAverageDifference(nums: number[]): number {
     let minDiff = -1;
     let indexOfMinDiff = 0;
 
@@ -9,9 +9,12 @@ function minimumAverageDifference(nums: number[]): number {
 
     for(let i=0; i<nums.length; i++){
         const leftAverage = Math.floor(prefix[i] / (i+1));
-        const rightAverage = Math.floor((prefix[prefix.length-1] - prefix[i]) / (prefix.length-(i+1)));
+        let rightAverage = 0;
+        if(i !== nums.length-1){
+            rightAverage = Math.floor((prefix[prefix.length-1] - prefix[i]) / (prefix.length-(i+1)));
+        }
         
-        const diff = Math.abs(leftAverage-(isNaN(rightAverage) ? 0 : rightAverage));
+        const diff = Math.abs(leftAverage-rightAverage);
         if(diff < minDiff || minDiff === -1){
             minDiff = diff;
             indexOfMinDiff = i;
