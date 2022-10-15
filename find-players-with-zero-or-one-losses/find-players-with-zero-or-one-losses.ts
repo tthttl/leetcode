dfba993339ddbfed10c8c3893d4ad0dd11018deb
Tóth Attila <tthttl@gmail.com>
2022-10-15 17:06:46 +0200

    Time: 670 ms, Memory: 103.4 MB - LeetHub

--- find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.ts
+++ find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.ts
@@ -1,20 +1,23 @@
 function findWinners(matches: number[][]): number[][] {
-    const winners = {};
+    const players = new Set<number>();
     const losers = {};
     const ans:number[][]=[[], []];
     for(const match of matches){
-        winners[match[0]]=true;
+        players.add(match[0]);
+        players.add(match[1]);
         losers[match[1]] = (losers[match[1]]||0) +1
     }
-    for(const winner of Object.keys(winners)){
-        if(!losers[winner]){
-            ans[0].push(Number(winner));
+    for(const player of players.values()){
+        const losses = losers[player];
+        if(!losses){
+            ans[0].push(player);
+        }
+        if(losses===1){
+            ans[1].push(player);
         }
     }
-    for(const loser of Object.keys(losers)){
-        if(losers[loser]===1){
-            ans[1].push(Number(loser));
-        }
+    for(const group of ans){
+        group.sort((a,b)=>a-b);
     }
     return ans;
 };
