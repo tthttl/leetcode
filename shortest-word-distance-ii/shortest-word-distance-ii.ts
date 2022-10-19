e3c5aa2688414a5314d5dafeeb008c36365a688a
Tóth Attila <tthttl@gmail.com>
2022-10-19 22:08:43 +0200

    Time: 164 ms, Memory: 49.9 MB - LeetHub

---
+++ shortest-word-distance-ii/shortest-word-distance-ii.ts
@@ -0,0 +1,30 @@
+class WordDistance {
+    
+    map = new Map<string, number[]>();
+    
+    constructor(wordsDict: string[]) {
+        for(let i=0; i<wordsDict.length; i++){
+            if(!this.map.has(wordsDict[i])){
+                this.map.set(wordsDict[i], []);   
+            }
+            const arr = this.map.get(wordsDict[i]);
+            arr!.push(i);
+            this.map.set(wordsDict[i], arr || []);
+        }
+    }
+
+    shortest(word1: string, word2: string): number {
+        let min = Infinity;
+        for(let i=0; i < this.map.get(word1).length; i++){
+            for(let j=0; j < this.map.get(word2).length; j++){
+                min = Math.min(min, Math.abs(this.map.get(word1)[i] - this.map.get(word2)[j]));
+            }
+        }
+        return min;   
+    }
+}
+/**
+ * Your WordDistance object will be instantiated and called as such:
+ * var obj = new WordDistance(wordsDict)
+ * var param_1 = obj.shortest(word1,word2)
+ */
