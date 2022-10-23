4cffe79d0192d4f28a6c5e0253df30b7add0ccb5
Tóth Attila <tthttl@gmail.com>
2022-10-23 17:38:00 +0200

    Time: 538 ms (90.91%), Space: 61.4 MB (9.09%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.ts
@@ -0,0 +1,23 @@
+class StockSpanner {
+  stack: number[][] = [[]]; //price, answer
+  constructor() {}
+
+  next(price: number): number {
+    let ans = 1;
+
+    while(this.stack.length && this.stack[this.stack.length - 1][0] <= price){
+        const last = this.stack.pop();
+        if(last){
+            ans += last[1];
+        }
+    }
+
+    this.stack.push([price, ans]);
+    return ans;
+  }
+}
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
