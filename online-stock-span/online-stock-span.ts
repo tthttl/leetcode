30ec142b452793a6c38cf70b70f4ffcd19d701e7
Tóth Attila <tthttl@gmail.com>
2022-10-23 13:58:41 +0200

    Time: 9538 ms, Memory: 60.1 MB - LeetHub

--- online-stock-span/online-stock-span.ts
+++ online-stock-span/online-stock-span.ts
@@ -1,23 +1,19 @@
 class StockSpanner {
-    stack: number[] = [];
-    numOfConsLessItems = 0;
-    constructor() {}
+  stack: number[] = [];
+  constructor() {}
 
-    next(price: number): number {
-        this.stack.push(price);
-        let pos = this.stack.length-1;
-        if(price > this.stack[pos]){
-            return this.numOfConsLessItems++;
-        } else {
-            this.numOfConsLessItems = 0;
-            while(this.stack.length && this.stack[pos] <= price){
-                this.numOfConsLessItems++
-                pos--;
-            }
-            return this.numOfConsLessItems;
-        }
+  next(price: number): number {
+    this.stack.push(price);
+    let pos = this.stack.length - 1;
+    let ans = 0;
+    while (this.stack.length && this.stack[pos] <= price) {
+      ans++;
+      pos--;
     }
+    return ans;
+  }
 }
+
 /**
  * Your StockSpanner object will be instantiated and called as such:
  * var obj = new StockSpanner()
