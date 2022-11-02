dcb173f6766a5f558d3ef5117ec639122dd02f68
Tóth Attila <tthttl@gmail.com>
2022-11-02 21:07:11 +0100

    Time: 750 ms (70.00%), Space: 136.7 MB (70.00%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.ts
@@ -0,0 +1,37 @@
+function minReorder(n: number, connections: number[][]): number {
+  //convert array of edges to undirected graph (hashmap of nodes as keys with their neighbours as array values)
+  const graph = new Map<number, number[]>();
+  const originalEdges = new Set<string>();
+  for (let i = 0; i < n; i++) {
+    graph.set(i, []);
+  }
+  for (const connection of connections) {
+    const [x, y] = connection;
+    graph.get(x)?.push(y);
+    graph.get(y)?.push(x);
+    originalEdges.add(`${x}, ${y}`);
+  }
+
+  //traverse graph AWAY from 0 node => loop through neighbours and traverse their neighbours
+  //make sure a node is only visitited once
+
+  const seen = new Set<number>();
+  seen.add(0);  
+  const dfs = (node: number): number => {
+    let ans = 0;
+    for(const neighbour of graph.get(node) || []){
+      if(!seen.has(neighbour)){
+        //check if current edge is in original graph => if yes, it means it has to be swapped, so its directed TOWARDS 0
+        if(originalEdges.has(`${node}, ${neighbour}`)){
+          ans++;
+        }
+        seen.add(neighbour);
+        ans += dfs(neighbour);
+      }
+    }
+    return ans;
+  }
+
+  return dfs(0);
+
+}
