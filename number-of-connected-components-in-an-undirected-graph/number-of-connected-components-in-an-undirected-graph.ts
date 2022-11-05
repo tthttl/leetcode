401261cf611691cef631cf395fd5bc64dd64feef
Tóth Attila <tthttl@gmail.com>
2022-11-05 20:54:46 +0100

    Time: 100 ms, Memory: 47.9 MB - LeetHub

---
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.ts
@@ -0,0 +1,36 @@
+function countComponents(n: number, edges: number[][]): number {
+  //create graph
+  const graph = new Map<number, number[]>();
+  for(let i = 0; i<n; i++){
+    graph.set(i, []);
+  }
+  for(const edge of edges){
+    graph.get(edge[0])?.push(edge[1]);
+    graph.get(edge[1])?.push(edge[0]);
+  }
+
+
+  const seen = new Set();
+  let ans = 0;
+  //map out a group
+  const dfs = (node): void => {
+    for(let neighbor of graph.get(node) || []){
+      if(!seen.has(neighbor)){
+        seen.add(neighbor);
+        dfs(neighbor);
+      }
+    }
+  }
+
+  //count number of groups
+  while(seen.size<n){
+    ans++;
+    const nextNode = [...graph.keys()].find((node: number) => !seen.has(node));
+    if(Array.isArray(graph.get(nextNode)) && !graph.get(nextNode)?.length){ //nodes with no edges
+      seen.add(nextNode);
+    }
+    dfs(nextNode);
+  }
+
+  return ans;
+};
