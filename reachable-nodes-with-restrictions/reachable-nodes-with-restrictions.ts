d56dbb4b16e759609dba9fb25ee9677fbb375651
Tóth Attila <tthttl@gmail.com>
2022-11-06 13:06:58 +0100

    Time: 677 ms, Memory: 148.2 MB - LeetHub

--- reachable-nodes-with-restrictions/reachable-nodes-with-restrictions.ts
+++ reachable-nodes-with-restrictions/reachable-nodes-with-restrictions.ts
@@ -4,19 +4,18 @@ function reachableNodes(n: number, edges: number[][], restricted: number[]): num
   for(let i=0; i<n; i++){
     graph.set(i, []);
   }
+  const restrictedSet = new Set(restricted);
   for(const edge of edges){
-    graph.get(edge[0])?.push(edge[1]);
-    graph.get(edge[1])?.push(edge[0]);
+    if(!restrictedSet.has(edge[0]) && !restrictedSet.has(edge[1])){
+        graph.get(edge[0])?.push(edge[1]);
+        graph.get(edge[1])?.push(edge[0]);
+    }
   }
 
   //create seen 
   const seen = new Set<number>();
 
   const dfs = (node: number, size: number): number => {
-    if(restricted.includes(node)){
-      seen.add(node);
-      return size;
-    }
     size++;
     for(const neighbor of graph.get(node)|| []){
       if(!seen.has(neighbor)){
