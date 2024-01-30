21a362404bb477eaba609dd9879f23df0dc9a986
Tóth Attila <tthttl@gmail.com>
2024-01-30 20:15:20 +0100

    Time: 62 ms, Memory: 51.5 MB - LeetHub

--- largest-unique-number/largest-unique-number.ts
+++ largest-unique-number/largest-unique-number.ts
@@ -1,10 +1,43 @@
 function largestUniqueNumber(nums: number[]): number {
-    const map = new Map();
-
-    for(let i=0; i<nums.length; i++){
-        map.set(nums[i], (map.get(nums[i])||0)+1);
+  
+    const occurences = new Map<number, number>();
+    
+    for(let num of nums){
+      occurences.set(num, (occurences.get(num) || 0)+1);
     }
     
-    const result = [...map].sort((a,b)=> b[0] - a[0]).find((entry) => entry[1]===1);
-    return result ? result[0] : -1;    
+    let max = Number.NEGATIVE_INFINITY;
+    
+    for(let entry of occurences.entries()){
+        if(entry[1] === 1){
+            max = Math.max(max, entry[0]);
+        }
+    }
+    
+    return max === Number.NEGATIVE_INFINITY ? -1 : max;
+    
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
