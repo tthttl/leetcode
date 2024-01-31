1605cfa0fddd345fb8c26f3b33cfc59e59e02465
Tóth Attila <tthttl@gmail.com>
2024-01-31 20:52:38 +0100

    Time: 81 ms, Memory: 58.6 MB - LeetHub

--- contiguous-array/contiguous-array.ts
+++ contiguous-array/contiguous-array.ts
@@ -1,26 +1,18 @@
 function findMaxLength(nums: number[]): number {
-    const map = new Map();
-    let ans = 0;
-    let count = 0;
+    const valuePerIndex = new Map<number, number>();
+    let value = 0;
+    let max = 0;
     
-    map.set(0,-1);
-    
-    //save count as key in map for every distinct count encountered while increasing/decreasing, together with the current index as value 
-    //if same count value encountered again, do not overwrite the stored ones, but instead store difference of current index and stored index in ans => this is the length of equal 0s and 1s
+    valuePerIndex.set(0, -1);
     for(let i=0; i<nums.length; i++){
-        if(nums[i]===0) {
-            count--;
-        }
-        if(nums[i]===1){
-            count++;
-        }
-        if(map.has(count)){
-            ans = Math.max(ans, i-map.get(count));
+        value = value + (nums[i] === 1 ? 1 : -1);
+        if(valuePerIndex.has(value)){
+            max = Math.max(max, i - valuePerIndex.get(value));
         } else {
-            map.set(count, i);
+            valuePerIndex.set(value, i);
         }
     }
     
-    return ans;
+    return max;
 };
 
